517cb6a3324be4b2cae62ab9597c162b29280204
Coki78 <drizki888@gmail.com>
2023-04-10 16:08:48 +0700

    update master

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Logo from '../../assets/Logo.png'
+import Nav from '../Nav/Nav'
+import NavMobile from '../NavMobile/NavMobile'
+
+
+function Header() {
+  return (
+    <div className=' container mx-a'>
+        HEADER
+    </div>
+  )
+}
+
+export default Header
